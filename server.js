5524b68d69391b7bdc476bd20a5c1013759db30f
Harshwardhan Vishwakarma <72744497+Avyoraj@users.noreply.github.com>
2024-06-08 17:09:14 +0530

    update server.js

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Serve static files from the main directory
+app.use(express.static(path.join(__dirname)));
+
 app.get('/', (req, res) => {
-  res.send('Hello, Azure Web App!');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {
